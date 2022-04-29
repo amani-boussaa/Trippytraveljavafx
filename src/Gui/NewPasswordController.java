9407537eb174107d9779b1900683a31afd3cef81
hamdi mechergui <mecherguihamdi45@gmail.com>
2022-04-29 06:21:52 +0100

    third commit

--- src/Gui/NewPasswordController.java
+++ src/Gui/NewPasswordController.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Gui;
 
+import Security.BCrypt;
 import Utils.MyDB;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
@@ -19,7 +15,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -29,26 +25,32 @@ import javafx.stage.Stage;
  *
  * @author meche
  */
-public class ActivationController implements Initializable {
+public class NewPasswordController implements Initializable {
 
     @FXML
-    private TextField tcode;
+    private TextField tmailCode;
     
     @FXML
-    private Label controleCode;
+    private PasswordField tnewPassword;
     
     @FXML
-    void activate(MouseEvent event) throws SQLException, IOException{
-        int testCode = Integer.parseInt(tcode.getText());
+    void updatePassword(MouseEvent event) throws IOException, SQLException {
+        int mailCode = Integer.parseInt(tmailCode.getText());
+        String newPassword = tnewPassword.getText();
+        String hashed = BCrypt.hashpw(newPassword, BCrypt.gensalt(12));
         int code = ActivationCode.getCode();
         String email = UserSession.getEmail();
-        if (testCode == code){
+        if (mailCode == code){
             Connection connection = MyDB.getInstance().getCon();
             Statement statement = connection.createStatement();
-            int status = statement.executeUpdate("UPDATE user SET roles='"+"ROLE_CLIENT"+"' WHERE email='"+email+"' ");
+            int status = statement.executeUpdate("UPDATE user SET password='"+hashed+"' WHERE email='"+email+"' ");
             if(status > 0){
-                System.out.println("activation success");
-                Parent root = FXMLLoader.load(getClass().getResource("loggedIn.fxml"));
+                System.out.println("update password success");
+                email=null;
+                String roles=null;
+                UserSession.getInstace(email, roles).cleanUserSession();
+                System.out.println(UserSession.getInstace(email, roles));
+                Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
                 Node node = (Node) event.getSource();
                 Stage stage = (Stage) node.getScene().getWindow();
                 stage.setScene(new Scene(root));
@@ -56,14 +58,13 @@ public class ActivationController implements Initializable {
                 System.out.println("sql error");
             }
         }else{
-            controleCode.setText("invalid code");
             System.out.println("code incorrecte");
         }
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
--- src/Gui/SignUpController.java
+++ src/Gui/SignUpController.java
@@ -1,5 +1,6 @@
 package Gui;
 
+import Security.BCrypt;
 import Utils.MyDB;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,6 +24,7 @@ import java.util.Properties;
 import java.util.Random;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javax.mail.Authenticator;
 import javax.mail.Message;
@@ -72,6 +74,7 @@ public class SignUpController implements Initializable {
             String lastname = tlastname.getText();
             String email = temail.getText();
             String password = tpassword.getText();
+            String hashed = BCrypt.hashpw(password, BCrypt.gensalt(12));
             
             if (firstname.length()<3){
                  firstnameControl.setText("3 caractères au minmum");
@@ -92,15 +95,18 @@ public class SignUpController implements Initializable {
             if(firstname.length()>2 && lastname.length()>2 && email.matches("(.*)@(.*)") && password.length()>5  ){
                 Statement statement = connection.createStatement();
                 int status = statement.executeUpdate("INSERT INTO `user`(`email`, `roles`, `password`, `is_verified`, `firstname`, `lastname`, `image`, `updated_at`, `bio`, `telephone`, `address`, `naissance`, `sexe`)"
-                    + "VALUES ('"+email+"','"+"ROLE_USER"+"','"+password+"','"+0+"','"+firstname+"','"+lastname+"','"+null+"','"+account_Date+"','"+null+"','"+null+"','"+null+"','"+account_Date+"','"+null+"' )");
+                    + "VALUES ('"+email+"','"+"ROLE_USER"+"','"+hashed+"','"+0+"','"+firstname+"','"+lastname+"','"+null+"','"+account_Date+"','"+null+"','"+null+"','"+null+"','"+account_Date+"','"+null+"' )");
 
                 if (status > 0) {
                     System.out.println("user registered");
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setHeaderText(null);
+                    alert.setContentText("Sign-Up success !");
+                    alert.showAndWait();
                     Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
                     Node node = (Node) event.getSource();
                     Stage stage = (Stage) node.getScene().getWindow();
                     stage.setScene(new Scene(root));
-                    this.sendMail(email);
                 }
             }
             
@@ -116,50 +122,6 @@ public class SignUpController implements Initializable {
         Node node = (Node) event.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         stage.setScene(new Scene(root));
-    }
-    
-    public static void sendMail(String recepient) throws Exception{
-        System.out.println("trying to send email");
-        
-        Properties properties = new Properties();
-        
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.smtp.host", "smtp.gmail.com");
-        properties.put("mail.smtp.port", "587");
-        
-        String senderEmail = "mecherguihamdi45@gmail.com";
-        String senderPassword = "ydgbqptuqmkktmjj";
-        
-        Session session = Session.getInstance(properties, new Authenticator(){
-           @Override
-           protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(senderEmail, senderPassword);
-            }
-        });
-        
-        Message message = prepareMessage(session, senderEmail, recepient);
-        Transport.send(message);
-        System.out.println("mail sent successfully");
-    }
-    
-    private static Message prepareMessage(Session session, String senderEmail, String recepient) {
-            
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(senderEmail));
-            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient));
-            message.setSubject("account verfication");
-            Random random = new Random();
-            int code = random.nextInt(99999999);
-            message.setText("code : " + code);
-            ActivationCode.getInstace(code);
-            System.out.println("instance code = "+ActivationCode.getCode());
-            return message;
-        } catch(Exception ex){
-            ex.printStackTrace();
-        }
-        return null;
     }
     
     @Override
